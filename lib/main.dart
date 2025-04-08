c4f70a5fd4eaed62b83466532f392d63c51294a8
Abdelrahman Mohamed Ali <abdo.kaka538@gmail.com>
2025-04-08 22:18:25 +0200

    adding asset images

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:financial_app/widgets/customelevatedbutton.dart';
 import 'package:financial_app/widgets/customtextfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const FinancialApp());
@@ -14,19 +15,16 @@ class FinancialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-  designSize: const Size(1034, 613), 
-  minTextAdapt: true,
-  builder: (context, child) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Customtextfield()
-        ),
-      ),
+      designSize: const Size(1034, 613),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          home: Scaffold(
+            body: Center(child: SvgPicture.asset('assets/images/google_ic.svg')),
+          ),
+        );
+      },
+      child: const SizedBox(), // 👈 ADD THIS!
     );
-  },
-  child: const SizedBox(), // 👈 ADD THIS!
-);
-
   }
 }
